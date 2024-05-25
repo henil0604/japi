5fa8afffc611fbc9cc931bab843d40ab4ba3dff4
Henil Malaviya <hmalaviya@ssessa.com>
2024-05-25 11:13:01 +0530

    feat(trpc): add new trpc related files and setup basic trpc server structure

---
+++ src/lib/server/trpc/index.ts
@@ -0,0 +1,24 @@
+import { initTRPC, type inferAsyncReturnType } from '@trpc/server';
+import type { RequestEvent } from '@sveltejs/kit';
+
+export type Context = inferAsyncReturnType<typeof createContext>;
+
+/*
+	what ever you return from this function will be available in `ctx` param in t.query function
+*/
+export async function createContext(event: RequestEvent) {
+	return {
+		event
+	};
+}
+
+const t = initTRPC.context<Context>().create();
+
+export const router = t.router;
+
+export const publicProcedure = t.procedure;
+
+export const privateProcedure = t.procedure.use(({ next }) => {
+	// TODO: implement logic
+	return next();
+});
