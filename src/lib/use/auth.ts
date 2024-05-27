11041e90fe03c7bd09c821da17354f3e07607e05
Henil Malaviya <henilm.dev@gmail.com>
2024-05-27 23:30:24 +0530

    feat(auth): implement getAuthorizationURL function
    the function can be used to generate authorization url by passing a "provider"

--- src/lib/use/auth.ts
+++ src/lib/use/auth.ts
@@ -1,22 +1,25 @@
-import { browser } from '$app/environment';
+import { goto } from '$app/navigation';
 import { AUTH_PROVIDER_ROUTES, NEXT_REDIRECT_SEARCH_PARAMETER_NAME } from '$lib/const/auth';
 import { ROUTES } from '$lib/const/routes';
+import type { AuthProvider } from '$lib/types/auth';
 
-export function signIn(
-	provider: keyof typeof AUTH_PROVIDER_ROUTES,
-	options?: {
+interface AuthorizationURLOptions {
 		redirectTo?: string;
 	}
-) {
-	if (!browser) return;
-
+export function getAuthorizationURL(provider: AuthProvider, options?: AuthorizationURLOptions) {
 	const url = AUTH_PROVIDER_ROUTES[provider];
 
 	if (options?.redirectTo) {
 		url.searchParams.set(NEXT_REDIRECT_SEARCH_PARAMETER_NAME, options.redirectTo);
 	}
 
-	location.href = url.href;
+	return url;
+}
+
+export function signIn(...args: Parameters<typeof getAuthorizationURL>) {
+	const url = getAuthorizationURL(...args);
+
+	goto(url);
 }
 
 export function signOut(redirectTo?: string) {
@@ -31,6 +34,7 @@ export function signOut(redirectTo?: string) {
 }
 
 export const useAuth = {
+	getAuthorizationURL,
 	signIn,
 	signOut
 };
