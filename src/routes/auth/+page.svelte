fe7a7226923c82dda1daa80bef2693b49862553b
Henil Malaviya <henilm.dev@gmail.com>
2024-05-27 23:34:42 +0530

    refactor(ui): use svelte snippet to render button

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
+	import type { AuthProvider } from '$lib/types/auth';
 	import { ICONS } from '$lib/const/ui';
 	import { useAuth } from '$lib/use/auth';
 	import Icon from '@iconify/svelte';
 </script>
 
+{#snippet AuthButton(icon: string, provider: AuthProvider)}
+	<Button href={useAuth.getAuthorizationURL(provider).href} class="gap-2" size="lg">
+		<Icon font-size={23} {icon} />
+		<span>
+			Continue with <span class="capitalize">{provider}</span>
+		</span>
+	</Button>
+{/snippet}
+
 <div class="wrapper">
-	<Button class="gap-2" size="lg" on:click={() => useAuth.signIn('github')}>
-		<Icon font-size={23} icon={ICONS.BRAND.GITHUB} />
-		Continue with Github</Button
-	>
-	<Button class="gap-2" size="lg" on:click={() => useAuth.signIn('google')}>
-		<Icon font-size={23} icon={ICONS.BRAND.GOOGLE} />
-		Continue with Google</Button
-	>
+	{@render AuthButton(ICONS.BRAND.GOOGLE, 'google')}
+	{@render AuthButton(ICONS.BRAND.GITHUB, 'github')}
 </div>
 
 <style>
