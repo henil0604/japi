599855021164e36e09ac6efed9f9333316bcd3c5
Henil Malaviya <henilm.dev@gmail.com>
2024-06-01 19:26:22 +0530

    chore(const): add more icons

--- src/lib/const/ui.ts
+++ src/lib/const/ui.ts
@@ -2,5 +2,8 @@ export const ICONS = {
 	BRAND: {
 		GOOGLE: 'mdi:google',
 		GITHUB: 'mdi:github'
-	}
+	},
+	LIGHTNING_FILLED: 'ph:lightning-fill',
+	TIMER_FILLED: 'basil:timer-solid',
+	SERVER_FILLED: 'mdi:server'
 };
