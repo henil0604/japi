eeadfae37cd4a4a4e819ed6584fe96099b06502f
Henil Malaviya <henilm.dev@gmail.com>
2024-06-01 12:59:27 +0530

    chore: use slot

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,10 @@
 <script>
 	import DefaultLayout from '$layouts/default.svelte';
 	import '$lib/styles/global.css';
-
-	const { children } = $props();
 </script>
 
 <DefaultLayout>
-	{@render children()}
+	<slot />
 </DefaultLayout>
 
 <style></style>
