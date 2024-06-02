a2a96c5c982126e419dc3c64d2bbb78e156d982e
Henil Malaviya <henilm.dev@gmail.com>
2024-06-02 22:19:29 +0530

    feat(ui): try out section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
 	import { Button } from '$lib/components/ui/button';
 	import { ICONS } from '$lib/const/ui';
 	import Icon from '@iconify/svelte';
+	import { JSONEditor, Mode, type Content } from 'svelte-jsoneditor';
+	import { writable, type Writable } from 'svelte/store';
 
 	const features = [
 		{
@@ -29,6 +31,58 @@
 				'The Service is fully open source on github. You can take a look at code if you think we are liers.'
 		}
 	];
+
+	const webEditorFeatures = [
+		'Simple',
+		'Syntax Highlighting',
+		'Multi mode view',
+		'Transformers',
+		'Quick Search',
+		'Formatting',
+		'Regex Search Support',
+		'Advance Search with Queries'
+	];
+
+	let tryoutEditorContent: Writable<Content> = writable({
+		json: {
+			what: 'Easy and simple JSON Bins',
+			butWhy: [...features.map((feature) => feature.title)],
+			freeTier: true
+		}
+	});
+
+	let tryoutEditorRef: JSONEditor;
+
+	// TODO: abstract this out
+	$: isTryoutEditorValueInvalid =
+		tryoutEditorRef && $tryoutEditorContent && tryoutEditorRef.validate() === null
+			? false
+			: true;
+
+	let temporaryBinId = null;
+
+	// TODO: abstract this out
+	function getContent() {
+		const content = tryoutEditorRef.get();
+
+		let json = 'json' in content === false ? JSON.parse(content.text) : content.json;
+
+		return {
+			json,
+			text: JSON.stringify(json, null, 4)
+		};
+	}
+
+	function handleTryoutEditorSaveAction() {
+		let content = getContent();
+
+		// format the editor
+		$tryoutEditorContent = {
+			text: content.text
+		};
+
+		// TODO: implement logic
+	}
 </script>
 
 <main>
@@ -77,6 +131,60 @@
 					</div>
 				</div>
 			{/each}
+		</div>
+	</div>
+
+	<div class="tryout-section">
+		<h1 class="heading">Try it out!</h1>
+
+		<div class="my-8" />
+		<div class="section">
+			<div class="editor-wrapper">
+				<JSONEditor
+					bind:this={tryoutEditorRef}
+					indentation={4}
+					bind:content={$tryoutEditorContent}
+					mode={Mode.text}
+				/>
+				<div class="action-footer">
+					<div class="action-info">
+						{#if temporaryBinId}
+							<div class="alert">
+								This temporary bin will be deleted after 24 hours. <a href=""
+									>Sign in to claim it
+								</a>.
+							</div>
+							<a href="" target="_blank" class="link"
+								>{location.origin}/api/{crypto.randomUUID()}</a
+							>
+						{:else}
+							<div class="create-info">Create a temporary public bin</div>
+						{/if}
+					</div>
+					<div class="action-button">
+						<Button
+							bind:disabled={isTryoutEditorValueInvalid}
+							on:click={handleTryoutEditorSaveAction}
+							>{temporaryBinId ? 'Save' : 'Create'}</Button
+						>
+					</div>
+				</div>
+			</div>
+			<div class="info-wrapper">
+				<div class="heading">Effortless Web Editing Experience</div>
+				<div class="features">
+					{#each webEditorFeatures as feature}
+						<div class="feature-item">
+							<div class="icon">
+								<Icon font-size={30} icon={ICONS.CHECK_CIRCLE} />
+							</div>
+							<div class="text">
+								{feature}
+							</div>
+						</div>
+					{/each}
+				</div>
+			</div>
 		</div>
 	</div>
 </main>
@@ -168,6 +276,120 @@
 					& .title {
 						font-size: theme('fontSize.2xl');
 						font-weight: theme('fontWeight.bold');
+					}
+				}
+			}
+		}
+	}
+
+	main .tryout-section {
+		--section-x-padding: clamp(theme('padding.20'), theme('padding.20'), theme('padding.40'));
+		--section-y-padding: clamp(theme('padding.10'), theme('padding.16'), theme('padding.40'));
+
+		width: 100%;
+		height: fit-content;
+		min-height: 80vh;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: var(--section-y-padding) var(--section-x-padding);
+
+		& .heading {
+			font-size: theme('fontSize.4xl');
+			font-weight: theme('fontWeight.bold');
+		}
+
+		& .section {
+			width: 100%;
+			height: 100%;
+			flex-grow: 1;
+			display: flex;
+			gap: theme('gap.8');
+
+			@media screen and (max-width: 1200px) {
+				flex-direction: column;
+
+				& .editor-wrapper,
+				.info-wrapper {
+					width: 100% !important;
+				}
+			}
+
+			& .editor-wrapper {
+				width: 50%;
+				min-height: 100%;
+				max-height: 70vh;
+
+				& .action-footer {
+					display: flex;
+					padding: theme('padding.4') 0;
+					justify-content: space-between;
+
+					& .action-info {
+						flex-grow: 1;
+						display: flex;
+						flex-direction: column;
+						justify-content: center;
+						gap: theme('gap.2');
+
+						& a {
+							text-decoration: underline;
+							text-underline-offset: theme('textUnderlineOffset.4');
+						}
+
+						& .alert {
+							font-size: theme('fontSize.sm');
+							color: theme('colors.red.600');
+							font-weight: theme('fontWeight.semibold');
+						}
+
+						& .link {
+							color: theme('colors.brand.DEFAULT');
+							filter: brightness(70%);
+							font-size: theme('fontSize.base');
+						}
+					}
+
+					& .action-button {
+						flex-grow: 0;
+						min-width: fit-content;
+					}
+				}
+			}
+
+			& .info-wrapper {
+				width: 50%;
+				display: flex;
+				flex-direction: column;
+				gap: theme('gap.4');
+
+				& .heading {
+					font-size: theme('fontSize.2xl');
+					font-weight: theme('fontWeight.bold');
+				}
+
+				& .features {
+					display: grid;
+					grid-template-columns: repeat(1, 1fr);
+					gap: theme('gap.4');
+
+					@media screen and (width <= 1200px) {
+						grid-template-columns: repeat(2, 1fr);
+					}
+
+					& .feature-item {
+						display: flex;
+						align-items: center;
+						gap: theme('gap.2');
+
+						& .icon {
+							color: theme('colors.brand.DEFAULT');
+						}
+
+						& .text {
+							font-size: theme('fontSize.lg');
+						}
 					}
 				}
 			}
